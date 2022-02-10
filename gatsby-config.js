88f977e97908b1ccabc5d5cd4cc16c5ba4aa34dc
Ersin Akinci <me@ersinakinci.com>
2022-02-09 21:16:48 -0700

    comment out GA plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,11 +31,11 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     // Add your Google Analytics ID to the .env file to enable
     // Otherwise, this plugin can be removed
-    process.env.GOOGLE_ANALYTICS_ID && {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
+    // process.env.GOOGLE_ANALYTICS_ID && {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: process.env.GOOGLE_ANALYTICS_ID,
+    //   },
+    // },
   ].filter(Boolean),
 }
